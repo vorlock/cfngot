736ee7151f131f9a5ed6b8085e27cd54625d5d74
Marcin Kulisz <marcin@bashton.com>
2015-06-01 14:10:46 +0100

    Fixing setuptools config

--- setup.py
+++ setup.py
@@ -13,16 +13,18 @@ def find_version(path):
 
 setup(name='cfngot',
       version=find_version(os.path.join(os.path.dirname(__file__), 'cfngot')),
-      packages=find_packages(),
-      include_package_data=True,
-      entry_points={'console_scripts': []},
+      description='Tool for generating cloudformation templates out of jinja2',
+      author='Marcin Kulisz',
+      author_email='sourceforge@kulisz.net',
+      url='http://www.python.org/sigs/distutils-sig/',
+      scripts=['cfngot'],
+      packages=['lib'],
       install_requires=['Jinaj2 >= 2.7.3',
                         'pyyaml >= 3.11',
                         'awscli >= 1.4.2',
                         'docopt >= 0.6.2',
                         ],
       license='The MIT License (MIT))',
-      description='Tool for generating cloudformation templates out of jinja2',
       long_description='''It's tool allowing for describing cloudformation
 templates with jinja2 formating. J2 templates will be then rendered to the pure
 json cloudformation templates.
@@ -30,7 +32,8 @@ This tool also allows for comparing two json files (json-diff subcommand),
 creating (new) and updating (already existing) CFN stacks on AWS accounts
 configured with profiles in awscli tool.
 ''',
-      author='Marcin Kulisz',
-      author_email='sourceforge@kulisz.net',
-      url='http://www.python.org/sigs/distutils-sig/',
+      entry_points={
+          'console_scripts': [
+              'cfngot = lib.main:main'],
+          },
      )
